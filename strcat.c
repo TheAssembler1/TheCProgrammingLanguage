f2863d2ee0eac3d5d4b234718069e765293230e9
TheAssembler1 <lewisnoah592@gmail.com>
2022-01-22 05:04:16 -0600

    Just more programs

---
+++ strcat.c
@@ -0,0 +1,11 @@
+//NOTE::strcat: concatenate t to end of s; s must be big enough
+
+void strcat(char s[], char t[]){
+    int i, j;
+
+    i = j = 0;
+    while(s[i] != '\0') //NOTE::Find end of s
+        i++;
+    while((s[i++] = t[j++]) != '\0')
+        ;
+}
